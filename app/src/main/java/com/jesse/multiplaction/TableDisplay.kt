0851f1b18247d796c06124aa3101978e04dbf669
JesseBennett-lab <j.bennett0505@gmail.com>
2026-04-01 16:53:12 +0200

    added string and comments

--- app/src/main/java/com/jesse/multiplaction/TableDisplay.kt
+++ app/src/main/java/com/jesse/multiplaction/TableDisplay.kt
@@ -1,6 +1,7 @@
 package com.jesse.multiplaction
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,15 @@ class TableDisplay : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // assigning data from main to variable
+        val bundle: Bundle? = intent.extras
+        //getting the number enter by user as string
+        val  tableString: String? = bundle?.getString("tableNumber")
+        val tableNumber = tableString?.toInt()
+        val tableDisplayText = findViewById<TextView>(R.id.tableDisplayText)
+
+
         setContentView(R.layout.activity_table_display)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
