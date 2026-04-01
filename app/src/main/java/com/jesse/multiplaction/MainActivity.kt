1f07f4df2cd341ffe2acedca2db5586eaf7fe70e
JesseBennett-lab <j.bennett0505@gmail.com>
2026-04-01 16:13:08 +0200

    updated multiply button

--- app/src/main/java/com/jesse/multiplaction/MainActivity.kt
+++ app/src/main/java/com/jesse/multiplaction/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.jesse.multiplaction
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val multiplyBtn = findViewById<Button>(R.id.multiplyBtn)
+        multiplyBtn.setOnClickListener {
+           val intent = Intent(this, TableDisplay::class.java)
+            startActivity(intent)
+        }
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
-}
